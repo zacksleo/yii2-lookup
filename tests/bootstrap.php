d3f184f6a16bad67226a938916ec3289f862774a
zhangjunwei <zhangjunwei@lianluo.com>
2017-07-28 18:10:57 +0800

    test

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,12 +9,12 @@ require_once(__DIR__ . '/../vendor/autoload.php');
 require_once(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
 
-//Yii::setAlias('@tests', __DIR__);
-//Yii::setAlias('@webroot', __DIR__);
-//Yii::setAlias('@web', '/');
+
+Yii::setAlias('@tests', __DIR__);
+Yii::setAlias('@webroot', __DIR__);
+Yii::setAlias('@web', '/');
 
 
 new \yii\console\Application([
-    'id' => 'unit',
-    'basePath' => __DIR__,
+    'id' => 'unit'
 ]);
