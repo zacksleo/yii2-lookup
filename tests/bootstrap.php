a5ce4176d11df7c10fa25eba3cf0cb57aef50244
zjw123 <1234@qq.com>
2017-07-29 14:42:52 +0800

    test

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,10 +7,4 @@ define('YII_DEBUG', true);
 require_once(__DIR__ . '/../vendor/autoload.php');
 require_once(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
-Yii::setAlias('@tests', __DIR__);
-Yii::setAlias('@webroot', __DIR__);
-Yii::setAlias('@web', '/');
-new \yii\console\Application([
-    'id' => 'unit',
-    'basePath' => __DIR__,
-]);
+
