fe78fe84a82ed6781c5afb8498f4869b70fea70a
zhangjunwei <zhangjunwei@lianluo.com>
2017-08-02 11:06:38 +0800

    test

---
+++ tests/layouts/main.php
@@ -0,0 +1,22 @@
+<?php
+
+
+/* @var \yii\web\View $this */
+/* @var string $content */
+
+
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+<head>
+    <meta charset="<?= Yii::$app->charset ?>"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+<?php $this->beginBody() ?>
+<?php echo $content; ?>
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
